eb315616688fe8dcf1830f04bc91991cd2db0274
Fahad Nadeem <nfahad4787@gmail.com>
2022-11-28 21:23:45 +0500

    half page

---
+++ common/header.php
@@ -0,0 +1,29 @@
+<header id="header">
+    <div class="container">
+        <a href="#" class="logo">
+            <img src="images/logo.png" alt="logo">
+        </a>
+
+
+        <div class="flex-box">
+            <nav id="nav">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Price calculator</a></li>
+                    <li><a href="#">Blogs</a></li>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+
+            <div class="auth">
+                <a href="#" class="btn"> Sign Up</a>
+                <a href="#" class="btn"> Sign In</a>
+            </div>
+        </div>
+
+
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,242 @@
+<?php
+$project_path = '';
+include_once $project_path . 'common/head.php';
+?>
+<div id="wrapper">
+    <?php
+    include_once $project_path . 'common/header.php';
+    ?>
+
+    <div class="banner">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="text-box">
+                        <h1>Cloud Computing Service</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore .</p>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="img-box">
+                        <img src="./images/banner1.png" alt="img">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section class="services">
+        <div class="container d-flex justify-content-between">
+            <div class="text-box">
+                <h1>Need Host Service?</h1>
+                <h4>You’re in the right place! <span>24x7 online support</span></h4>
+            </div>
+            <div class="text-box">
+                <h5>Call us toll free: </h5>
+                <h5><i class="fas fa-headphones"></i> <span>18000123123</span></h5>
+
+            </div>
+        </div>
+
+    </section>
+
+
+    <section class="offer">
+        <div class="container">
+            <div class="sec-heading">
+                <h2>What We Offers</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore .Lorem ipsum dolor sit </p>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="card">
+                        <div class="card-header">
+                            <h2>Dedicated Server</h2>
+                        </div>
+
+                        <div class="card-content">
+                            <ul class="info">
+                                <li>Intel Xeon D-2141 I (4.20 GHz Octa Core W/HT)</li>
+
+                                <li>32 GB DDR4 RAM</li>
+
+                                <li>1TB SSD (RAID 1)</li>
+                                
+                                <li>4 Dedicated IPv4</li>
+                                
+                                <li>Remote And VNC Access</li>
+
+                                <li>SSH Access On Linux</li>
+                                
+                                <li>Remote Connection On Windows</li>
+
+                                <li>12 TB Bandwidth</li>
+
+                                <li>DDoS Protection Included</li>
+
+                                <li>All Linux Versions</li>
+
+                                <li>All Windows Versions</li>
+
+                                
+                            </ul>
+
+                            <span class="amount">INR 8995 / Month</span>
+                            <a href="#" class="btn">Get More</a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-lg-3">
+                    <div class="card">
+                        <div class="card-header">
+                            <h2>Dedicated Server</h2>
+                        </div>
+
+                        <div class="card-content">
+                            <ul class="info">
+                                <li>Intel Xeon D-2141 I (4.20 GHz Octa Core W/HT)</li>
+
+                                <li>32 GB DDR4 RAM</li>
+
+                                <li>1TB SSD (RAID 1)</li>
+                                
+                                <li>4 Dedicated IPv4</li>
+                                
+                                <li>Remote And VNC Access</li>
+
+                                <li>SSH Access On Linux</li>
+                                
+                                <li>Remote Connection On Windows</li>
+
+                                <li>12 TB Bandwidth</li>
+
+                                <li>DDoS Protection Included</li>
+
+                                <li>All Linux Versions</li>
+
+                                <li>All Windows Versions</li>
+
+                                
+                            </ul>
+
+                            <span class="amount">INR 8995 / Month</span>
+                            <a href="#" class="btn">Get More</a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-lg-3">
+                    <div class="card">
+                        <div class="card-header">
+                            <h2>Dedicated Server</h2>
+                        </div>
+
+                        <div class="card-content">
+                            <ul class="info">
+                                <li>Intel Xeon D-2141 I (4.20 GHz Octa Core W/HT)</li>
+
+                                <li>32 GB DDR4 RAM</li>
+
+                                <li>1TB SSD (RAID 1)</li>
+                                
+                                <li>4 Dedicated IPv4</li>
+                                
+                                <li>Remote And VNC Access</li>
+
+                                <li>SSH Access On Linux</li>
+                                
+                                <li>Remote Connection On Windows</li>
+
+                                <li>12 TB Bandwidth</li>
+
+                                <li>DDoS Protection Included</li>
+
+                                <li>All Linux Versions</li>
+
+                                <li>All Windows Versions</li>
+
+                                
+                            </ul>
+
+                            <span class="amount">INR 8995 / Month</span>
+                            <a href="#" class="btn">Get More</a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-lg-3">
+                    <div class="card">
+                        <div class="card-header">
+                            <h2>Dedicated Server</h2>
+                        </div>
+
+                        <div class="card-content">
+                            <ul class="info">
+                                <li>Intel Xeon D-2141 I (4.20 GHz Octa Core W/HT)</li>
+
+                                <li>32 GB DDR4 RAM</li>
+
+                                <li>1TB SSD (RAID 1)</li>
+                                
+                                <li>4 Dedicated IPv4</li>
+                                
+                                <li>Remote And VNC Access</li>
+
+                                <li>SSH Access On Linux</li>
+                                
+                                <li>Remote Connection On Windows</li>
+
+                                <li>12 TB Bandwidth</li>
+
+                                <li>DDoS Protection Included</li>
+
+                                <li>All Linux Versions</li>
+
+                                <li>All Windows Versions</li>
+
+                                
+                            </ul>
+
+                            <span class="amount">INR 8995 / Month</span>
+                            <a href="#" class="btn">Get More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="text-box mt-5">
+                        <h3>About us</h3>
+                        <h2>Secure, Scalable , performance based and pay for resources</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore .Lorem ipsum dolor sit adipiscing elit, sed do eiusmod tempor incididunt ut labore .Lorem ipsum dolor sit </p>
+
+                        <a href="#" class="btn">Read More</a>
+                    </div>
+                </div>
+
+                <div class="col-md-5">
+                    <div class="img-box">
+                        <img src="./images/about-us.png" alt="img">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php
+    include_once $project_path . 'common/footer.php';
+    ?>
+</div>
+<?php
+include_once $project_path . 'common/end-page.php';
+?>
